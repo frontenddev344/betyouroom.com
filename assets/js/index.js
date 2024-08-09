f14f7be094b467a3a4840bc60c5f32bcf3886106
Dev avology <devavology12@gmail.com>
2024-08-09 12:00:50 +0530

    testing points done

--- assets/js/index.js
+++ assets/js/index.js
@@ -141,31 +141,6 @@ $(document).ready(function (e) {
 
 
 
-// jQuery(document).ready(function (e) {
-//   function t(t) {
-//       e(t).bind("click", function (t) {
-//           t.preventDefault();
-//           e(this).parent().fadeOut()
-//       })
-//   }
-//   e(".dropdown-toggle").click(function () {
-//       var t = e(this).parents(".button-dropdown").children(".dropdown").is(":hidden");
-//       e(".button-dropdown .dropdown").hide();
-//       e(".button-dropdown .dropdown-toggle").removeClass("dropping");
-//       if (t) {
-//           e(this).parents(".button-dropdown").children(".dropdown").toggle().parents(".button-dropdown").children(".dropdown-toggle").addClass("dropping")
-//       }
-//   });
-//   e(document).bind("click", function (t) {
-//       var n = e(t.target);
-//       if (!n.parents().hasClass("button-dropdown")) e(".button-dropdown .dropdown").hide();
-//   });
-//   e(document).bind("click", function (t) {
-//       var n = e(t.target);
-//       if (!n.parents().hasClass("button-dropdown")) e(".button-dropdown .dropdown-toggle").removeClass("dropping");
-//   })
-// });
-
 $(document).ready(function(){
   $(".button-dropdown").click(function(){
     $(this).find(".dropdown").slideToggle();
